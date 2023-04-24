1b6e782822ac2feb001a6daa95de1e253f393860
RZFury <48477044+rzfury@users.noreply.github.com>
2023-04-24 14:30:20 +0700

    fix: eslilint

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -93,7 +93,7 @@ export default function PP() {
       </ul>
       <H3>Usage Data</H3>
       <P>Usage Data is collected automatically when using the Service.</P>
-      <P>Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</P>
+      <P>Usage Data may include information such as Your Device&#39;s Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</P>
       <P>When You access the Service by or through a mobile device, We may collect certain information automatically, including, but not limited to, the type of mobile device You use, Your mobile device unique ID, the IP address of Your mobile device, Your mobile operating system, the type of mobile Internet browser You use, unique device identifiers and other diagnostic data.</P>
       <P>We may also collect information that Your browser sends whenever You visit our Service or when You access the Service by or through a mobile device.</P>
       <H3>Information from Third-Party Social Media Services</H3>
@@ -104,8 +104,8 @@ export default function PP() {
         <li>Twitter</li>
         <li>LinkedIn</li>
       </ul>
-      <P>If You decide to register through or otherwise grant us access to a Third-Party Social Media Service, We may collect Personal data that is already associated with Your Third-Party Social Media Service's account, such as Your name, Your email address, Your activities or Your contact list associated with that account.</P>
-      <P>You may also have the option of sharing additional information with the Company through Your Third-Party Social Media Service's account. If You choose to provide such information and Personal Data, during registration or otherwise, You are giving the Company permission to use, share, and store it in a manner consistent with this Privacy Policy.</P>
+      <P>If You decide to register through or otherwise grant us access to a Third-Party Social Media Service, We may collect Personal data that is already associated with Your Third-Party Social Media Service&#39;s account, such as Your name, Your email address, Your activities or Your contact list associated with that account.</P>
+      <P>You may also have the option of sharing additional information with the Company through Your Third-Party Social Media Service&#39;s account. If You choose to provide such information and Personal Data, during registration or otherwise, You are giving the Company permission to use, share, and store it in a manner consistent with this Privacy Policy.</P>
       <BR/>
       <H2>Use of Your Personal Data</H2>
       <P>The Company may use Personal Data for the following purposes:</P>
@@ -120,7 +120,7 @@ export default function PP() {
           <P><strong>For the performance of a contract:</strong> the development, compliance and undertaking of the purchase contract for the products, items or services You have purchased or of any other contract with Us through the Service.</P>
         </li>
         <li>
-          <P><strong>To contact You:</strong> To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application's push notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when necessary or reasonable for their implementation.</P>
+          <P><strong>To contact You:</strong> To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application&#39;s push notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when necessary or reasonable for their implementation.</P>
         </li>
         <li>
           <P><strong>To provide You</strong> with news, special offers and general information about other goods, services and events which we offer that are similar to those that you have already purchased or enquired about unless You have opted not to receive such information.</P>
@@ -150,7 +150,7 @@ export default function PP() {
       <P>The Company will also retain Usage Data for internal analysis purposes. Usage Data is generally retained for a shorter period of time, except when this data is used to strengthen the security or to improve the functionality of Our Service, or We are legally obligated to retain this data for longer time periods.</P>
       <BR/>
       <H2>Transfer of Your Personal Data</H2>
-      <P>Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.</P>
+      <P>Your information, including Personal Data, is processed at the Company&#39;s operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.</P>
       <P>Your consent to this Privacy Policy followed by Your submission of such information represents Your agreement to that transfer.</P>
       <P>The Company will take all steps reasonably necessary to ensure that Your data is treated securely and in accordance with this Privacy Policy and no transfer of Your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of Your data and other personal information.</P>
       <BR/>
@@ -179,13 +179,13 @@ export default function PP() {
       <P>The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.</P>
       <BR/>
       <BR/>
-      <H1>Children's Privacy</H1>
+      <H1>Children&#39;s Privacy</H1>
       <P>Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.</P>
-      <P>If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.</P>
+      <P>If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent&#39;s consent before We collect and use that information.</P>
       <BR/>
       <BR/>
       <H1>Links to Other Websites</H1>
-      <P>Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.</P>
+      <P>Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party&#39;s site. We strongly advise You to review the Privacy Policy of every site You visit.</P>
       <P>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</P>
       <BR/>
       <BR/>
